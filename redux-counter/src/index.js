fb48c76bd75d0b047054291cc90e8cdd9b76ec85
zarrko <zachariah.mwangi@imbank.co.ke>
2019-10-10 14:42:24 +0300

    feat(redux): simple counter app using redux

---
+++ redux-counter/src/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+import './styles.scss';
+import { INCREMENT, DECREMENT } from './constants';
+
+const initialState = {
+  count: 0,
+};
+
+// Action Creators
+const increment = () => ({
+  type: INCREMENT,
+})
+
+const decrement = () => ({
+  type: DECREMENT,
+})
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    };
+  } else if (action.type === DECREMENT) {
+    return {
+      count: state.count - 1,
+    }
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+class Counter extends Component {
+  render() {
+    const { count, increment, decrement } = this.props
+    return (
+      <main className="Counter">
+        <p className="count">{count}</p>
+        <section className="controls">
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => { return state };
+const mapDispatchToProps = {
+  increment,
+  decrement
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter)
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>
+  , document.getElementById('root')
+);
